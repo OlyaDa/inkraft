4673e6b8f8ec4b7edd7a6ccf83d4b814075649b5
ola <dashevska.olga@gmail.com>
2020-04-01 09:10:12 +0300

    add image popup

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,15 @@
 //mobile menu (header)
 
-$(function(){
-    $('.toggle-menu').on('click',function(){
+$(function () {
+    $('.toggle-menu').on('click', function () {
         $('.main-nav').toggleClass('open');
         $('.header').toggleClass('bg');
-        $( ".toggle-menu" ).toggleClass( "menu-on");
+        $(".toggle-menu").toggleClass("menu-on");
     });
 });
 
-$(document).ready(function() {
-    $(document).ready(function() {
+$(document).ready(function () {
+    $(document).ready(function () {
         $('.nav-link-new').click(function () {
             $('.main-nav').removeClass('open');
             $('.toggle-menu').removeClass('menu-on');
@@ -20,30 +20,74 @@ $(document).ready(function() {
 
 //mobile menu (header)
 
-$(window).scroll(function() {
+$(window).scroll(function () {
     let height = $(window).scrollTop();
-    if(height > 1){
+    if (height > 1) {
         $('.header').addClass('color');
-    } else{
+    } else {
         $('.header').removeClass('color');
     }
 });
 
 //input mask
 
-$('.phoneidcl').inputmask('+380(99) 999-99-99',{ "oncomplete": function(){
+$('.phoneidcl').inputmask('+380(99) 999-99-99', {
+    "oncomplete": function () {
         $(".disbutt").prop("disabled", false);
-    }, "onincomplete": function(){ $(".disbutt").prop("disabled", true); } });
+    }, "onincomplete": function () {
+        $(".disbutt").prop("disabled", true);
+    }
+});
 
 //modal window
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('.popup-link').magnificPopup();
 });
 
 //scroll
 
 $('.nav-link-new').mPageScroll2id();
+
+//image popup
+
+$('.image-link').magnificPopup({
+    type: 'image'
+    // other options
+});
+
+//video popup
+
+$(document).ready(function () {
+    $('.popup-youtube').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+});
+
+//
+$(document).ready(function() {
+
+    $('.image-popup-no-margins').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        closeBtnInside: false,
+        fixedContentPos: true,
+        mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+        image: {
+            verticalFit: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300 // don't foget to change the duration also in CSS
+        }
+    });
+
+});
 
 //slider for "banner"
 
@@ -52,8 +96,8 @@ $('.banner-slider').slick({
     slidesToScroll: 1,
     infinite: true,
     arrows: true,
-    prevArrow:'<button class="slick-arrow slick-prev"> <span class="icon-ar-left"></span> </button>',
-    nextArrow:'<button class="slick-arrow slick-next"> <span class="icon-ar-right"></span> </button>',
+    prevArrow: '<button class="slick-arrow slick-prev"> <span class="icon-ar-left"></span> </button>',
+    nextArrow: '<button class="slick-arrow slick-next"> <span class="icon-ar-right"></span> </button>',
     dots: false,
     autoplay: false,
 
@@ -78,8 +122,8 @@ $('.doing-slider').slick({
     centerMode: true,
     centerPadding: '0px',
     autoplay: false,
-    prevArrow:'<button class="slick-arrow slick-prev"> <span class="icon-ar-left"></span> </button>',
-    nextArrow:'<button class="slick-arrow slick-next"> <span class="icon-ar-right"></span> </button>',
+    prevArrow: '<button class="slick-arrow slick-prev"> <span class="icon-ar-left"></span> </button>',
+    nextArrow: '<button class="slick-arrow slick-next"> <span class="icon-ar-right"></span> </button>',
     responsive: [
         {
             breakpoint: 768,
@@ -101,8 +145,8 @@ $('.work-slider').slick({
     dots: false,
     autoplay: false,
     autoplaySpeed: 2000,
-    prevArrow:'<button class="slick-arrow slick-prev"> <span class="icon-ar-left"></span> </button>',
-    nextArrow:'<button class="slick-arrow slick-next"> <span class="icon-ar-right"></span> </button>',
+    prevArrow: '<button class="slick-arrow slick-prev"> <span class="icon-ar-left"></span> </button>',
+    nextArrow: '<button class="slick-arrow slick-next"> <span class="icon-ar-right"></span> </button>',
     responsive: [
         {
             breakpoint: 500,
@@ -118,8 +162,8 @@ $('.work-slider').slick({
 $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    prevArrow:'<button class="slick-arrow slick-prev"> <span class="icon-ar-left"></span> </button>',
-    nextArrow:'<button class="slick-arrow slick-next"> <span class="icon-ar-right"></span> </button>',
+    prevArrow: '<button class="slick-arrow slick-prev"> <span class="icon-ar-left"></span> </button>',
+    nextArrow: '<button class="slick-arrow slick-next"> <span class="icon-ar-right"></span> </button>',
     fade: true,
     asNavFor: '.slider-nav',
     responsive: [
@@ -142,20 +186,6 @@ $('.slider-nav').slick({
     arrows: false,
 });
 
-//video popup
-
-$(document).ready(function() {
-    $('.popup-youtube').magnificPopup({
-        disableOn: 700,
-        type: 'iframe',
-        mainClass: 'mfp-fade',
-        removalDelay: 160,
-        preloader: false,
-        fixedContentPos: false
-    });
-});
-
-
 // setNameScroll
 
 const nameScroll = document.getElementById('input_order');
@@ -177,45 +207,27 @@ function setNameScroll(namescroll, mail) {
 
 //tele-order
 
-$(document).ready(function(){
-    $('.order-main').submit(function(e){
+$(document).ready(function () {
+    $('.order-main').submit(function (e) {
         e.preventDefault();
         $.magnificPopup.close();
         $(".disbutt").prop("disabled", true);
         $.ajax({
-            type:'POST',
-            url:'/wp-content/themes/baugroup/tele_order.php',
+            type: 'POST',
+            url: '/wp-content/themes/baugroup/tele_order.php',
             data: $(this).serialize()
-        }).done(function(){
+        }).done(function () {
             $.magnificPopup.close();
-            setTimeout(function(){
+            setTimeout(function () {
                 window.location = 'done';
-            },1);
+            }, 1);
         });
     });
 });
 
-//
+//скрипт, чтобы фиксировалась высота слайдера при табуляции
 
-// $(".nav-item").click(function(e) {
-//     $(".work-slider").slick('refresh');
-//     $(".work-slider").slick('slickPrev');
-//     console.log('refresh');
-// });
-
-// const refresh = function(){
-//     $(".work-slider").slick('refresh');
-// };
-//
-//
-// $(".nav-item").click(function(e) {
-//
-//     setTimeout(refresh, 400);
-//
-//     console.log('refresh');
-// });
-
-$(window).load(function() {
+$(window).load(function () {
     $('.work-slider').on('setPosition', function () {
         $(this).find('.slick-slide').height('auto');
         var slickTrack = $(this).find('.slick-track');
@@ -223,3 +235,5 @@ $(window).load(function() {
         $(this).find('.slick-slide').css('height', slickTrackHeight + 'px');
     });
 })
+
+
